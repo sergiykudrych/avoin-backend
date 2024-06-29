efd7520b03d88127a03d274ebaa3877ed10fd90e
Sergiy Kudrych <81011171+sergiykudrych@users.noreply.github.com>
2024-06-29 15:52:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    // origin: 'http://localhost:3000',
-    origin: process.env.CLIENT_URL,
+    
+    origin: "*",
   })
 );
 app.use(express.json({ extended: true }));
